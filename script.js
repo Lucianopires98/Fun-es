9edc4461bd4db522b84dbddf33b992cf4cbc38de
Luciano Pires <lucianopires98@outlook.com>
2022-06-18 14:49:37 -0300

    Funções

---
+++ script.js
@@ -0,0 +1,23 @@
+function nome () {
+    console.log("Meu nome é Luciano")
+}
+
+nome ()
+
+function name (nome) {
+    console.log("Bem vindo, ${nome}!")
+}
+
+nome("Julio")
+
+function about (nome,idade,estilo) {
+    console.log("Meu nome é ${nome}, tenho ${idade} anos e amo escutar ${estilo}!")
+}
+
+about ("Luciano,23,Rap")
+
+function musica (movie,music) {
+    console.log("Meu filme favorito é ${movie} e minha música favorita é ${music}!")
+}
+
+musica ("O chamado, De peça em peça")
